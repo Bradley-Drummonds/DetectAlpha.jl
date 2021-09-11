60262ca335f487911ed0dc8f21dacf8cc21c494d
Bradley Drummonds <bradrum@gmail.com>
2021-09-11 15:00:05 +0200

    added Radiation, DecayType s

--- src/DetectAlpha.jl
+++ src/DetectAlpha.jl
@@ -1,5 +1,13 @@
 module DetectAlpha
 
+@enum DecayType α β γ n
 # Write your package code here.
+struct Radiation
+    particle::Bool
+    type::DecayType
+    energy::Float32
+    Radiation() = new(true,α,6.5)
+end
 
+export Radiation,DecayType,α,β 
 end
