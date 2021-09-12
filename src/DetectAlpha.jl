a51005c435b7292e0691ebe69736b22d2979ced2
Bradley Drummonds <bradrum@gmail.com>
2021-09-12 10:03:27 +0200

    added CSV dependency  and Isotope struct

--- src/DetectAlpha.jl
+++ src/DetectAlpha.jl
@@ -1,13 +1,28 @@
 module DetectAlpha
 
+using CSV
+
+export Radiation 
+
 @enum DecayType α β γ n
 # Write your package code here.
 struct Radiation
-    particle::Bool
     type::DecayType
     energy::Float32
-    Radiation() = new(true,α,6.5)
+    Radiation() = new(α,6.5)
 end
 
-export Radiation,DecayType,α,β 
+elementsFile = CSV.File("/home/b/Data/Elements.csv")
+
+struct Isotope
+    Z
+    A
+    symbol::String
+    Isotope() = (Z = 92;A=238;symbol = "Pu")
+    function Isotope(z::Int,a::Int) 
+        isoRow = elementsDf[elementsDf.AtomicNumber .== z,:]
+        sym = first(isoRow).Symbol
+        new(z,a,sym)
+    end
+end
 end
