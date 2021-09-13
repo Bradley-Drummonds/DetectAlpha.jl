2cf5bc3f0ac502755aa766150a7d2627c945e8ee
Bradley Drummonds <bradrum@gmail.com>
2021-09-13 20:20:05 +0200

    added example test added using DataFrames Spectrum and AlphaSpectrm

--- src/DetectAlpha.jl
+++ src/DetectAlpha.jl
@@ -1,6 +1,7 @@
 module DetectAlpha
 
 using CSV
+using DataFrames
 include("Utils.jl")
 
 export Radiation, Elements, Isotope
@@ -54,28 +55,40 @@ struct Isotope
         new(z,a,sym,nme,active)
     end
 end
+function decay!(iso::Isotope,radType::DecayType)
+    if radType == α
+        newIso = Isotope(iso.Z-2,iso.A-4)
+    elseif radType == β
+        newIso = Isotope(iso.Z+1,iso.A)
+    elseif radType == n
+        newIso = Isotope(iso.Z,iso.A-1)
+    else
+        newIso = Isotope(iso.Z,iso.A)
+    end
+    return newIso
+end
 
-# abstract type Spectrum end
-# find_peak(s::Spectrum) = []
-# slice(s::Spectrum) = Spectrum()
+abstract type Spectrum end
+find_peak(s::Spectrum) = []
+slice(s::Spectrum) = Spectrum()
 
-# struct AlphaSpectrum <: Spectrum
-#     channels::Vector{Int}
-#     energies::Vector{Float64}
-#     function AlphaSpectrum()
-#         chs = zeros(Int32,1024)
-#         e = zeros(1024)
-#         new(chs,e)
-#     end
-#     function AlphaSpectrum(row::DataFrameRow)
-#         numChannels = length(row)
-#         @show numChannels
-#         chs = [row[i] for i=4:numChannels]
-#         RANGE_MEV = 10_000_000; 
-#         b = convert(Float64,RANGE_MEV) / convert(Float64,numChannels)
-#         es = [b * i for i=1:numChannels-3]
-#         new(chs,es)
-#     end
-# end
+struct AlphaSpectrum <: Spectrum
+    channels::Vector{Int}
+    energies::Vector{Float64}
+    function AlphaSpectrum()
+        chs = zeros(Int32,1024)
+        e = zeros(1024)
+        new(chs,e)
+    end
+    function AlphaSpectrum(row::DataFrameRow)
+        numChannels = length(row)
+        @show numChannels
+        chs = [row[i] for i=4:numChannels]
+        RANGE_MEV = 10_000_000; 
+        b = convert(Float64,RANGE_MEV) / convert(Float64,numChannels)
+        es = [b * i for i=1:numChannels-3]
+        new(chs,es)
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "DetectAlpha.jl" begin
     # Write your tests here.
+    @test π ≈ 3.14 atol=0.01
 end
