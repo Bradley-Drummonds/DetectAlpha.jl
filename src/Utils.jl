e321d172ca5b5d887325408f76e440452af9c49f
Bradley Drummonds <bradrum@gmail.com>
2021-10-31 13:03:44 +0100

    added chained inequality example

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,2 +1,4 @@
 export parsebool
+export between2and8
 parsebool(s::String) = lowercase(s) == "yes" ? true : false
+between2and8(x) = 2 < x < 8
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,13 @@ include("../src/Utils.jl")
 @testset "DetectAlpha.jl" begin
     # Write your tests here.
     @test π ≈ 3.14 atol=0.01
+end
+
+@testset "Utils.jl" begin
     @test parsebool("false") == false
     @test parsebool("yes") == true
+    @test between2and8(5) == true
+    @test between2and8(9) == false
+    @test between2and8(5//2) == true
+    @test between2and8(4//2) == false
 end
