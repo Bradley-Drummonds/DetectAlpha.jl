b8c6f150945a788af7b423da52bd142b69e07a08
Bradley Drummonds <bradrum@gmail.com>
2022-02-19 19:10:21 +0100

    put Isotope logic in Isotope.jl

--- src/DetectAlpha.jl
+++ src/DetectAlpha.jl
@@ -5,69 +5,8 @@ using DataFrames
 using RadiationSpectra
 
 include("Utils.jl")
+include("Isotope.jl")
 
-export Radiation, Elements, Isotope
-
-@enum DecayType α β γ n
-# Write your package code here.
-struct Radiation
-    type::DecayType
-    energy::Float32
-    Radiation() = new(α,6.5)
-end
-
-struct Elements
-    file
-    Elements() = new(CSV.File("/home/b/Data/Elements.csv"))
-end
-get_iso_row(z::Int,elems::Elements) = elems.file[elems.file.AtomicNumber .== z,:]
-get_iso_row(z::Int) = get_iso_row(z,Elements())
-get_iso_row(symbol::String,elems::Elements) = elems.file[elems.file.Symbol .== symbol,:] 
-
-struct Isotope
-    Z
-    A
-    symbol::String
-    name::String
-    radioactive::Bool
-    Isotope() = new(94,239,"Plutonium","Pu",true)
-    function Isotope(z::Int,a::Int) 
-        isoRow = get_iso_row(z)
-        result = first(isoRow)
-        sym = result.Symbol
-        nme = result.Element
-        wkStr = result.Radioactive
-        if wkStr !== nothing 
-            active = parsebool(String(wkStr))
-        end
-        a = result.AtomicMass
-        new(z,a,sym,nme,active)
-    end
-    function Isotope(z::Int)
-        isoRow = get_iso_row(z)
-        result = first(isoRow)
-        sym = result.Symbol
-        nme = result.Element
-        wkStr = result.Radioactive
-        if wkStr !== nothing 
-            active = parsebool(String(wkStr))
-        end
-        a = result.AtomicMass
-        new(z,a,sym,nme,active)
-    end
-end
-function decay!(iso::Isotope,radType::DecayType)
-    if radType == α
-        newIso = Isotope(iso.Z-2,iso.A-4)
-    elseif radType == β
-        newIso = Isotope(iso.Z+1,iso.A)
-    elseif radType == n
-        newIso = Isotope(iso.Z,iso.A-1)
-    else
-        newIso = Isotope(iso.Z,iso.A)
-    end
-    return newIso
-end
 
 abstract type Spectrum end
 find_peak(s::Spectrum) = []
@@ -108,11 +47,13 @@ end
 "read and return an array of alpha spectra from a CSV file"
 function read_alpha_spectrum_file(file)
     frame = CSV.File(file; header=false) |> DataFrame
-    num_rows = DataAPI.nrow(frame)
+    num_rows = nrow(frame)
     ret_alpha_spects = [AlphaSpectrum(frame[i,:]) for i in 1:num_rows]
     return ret_alpha_spects
 end
 
 function find_peak(as::AlphaSpectrum)
-
+    println("find an alpha peak in spectrum ",as)
 end
+
+end #end module DetectAlpha
---
+++ src/Isotope.jl
@@ -0,0 +1,66 @@
+
+
+export Radiation, Elements, Isotope
+
+@enum DecayType α β γ n
+# Write your package code here.
+struct Radiation
+    type::DecayType
+    energy::Float32
+    Radiation() = new(α,6.5)
+end
+
+struct Elements
+    file
+    Elements() = new(CSV.File("/home/b/Data/Elements.csv"))
+end
+get_iso_row(z::Int,elems::Elements) = elems.file[elems.file.AtomicNumber .== z,:]
+get_iso_row(z::Int) = get_iso_row(z,Elements())
+get_iso_row(symbol::String,elems::Elements) = elems.file[elems.file.Symbol .== symbol,:] 
+
+struct Isotope
+    Z
+    A
+    symbol::String
+    name::String
+    radioactive::Bool
+    Isotope() = new(94,239,"Plutonium","Pu",true)
+    function Isotope(z::Int,a::Int) 
+        isoRow = get_iso_row(z)
+        result = first(isoRow)
+        sym = result.Symbol
+        nme = result.Element
+        wkStr = result.Radioactive
+        if wkStr !== nothing 
+            active = parsebool(String(wkStr))
+        end
+        a = result.AtomicMass
+        new(z,a,sym,nme,active)
+    end
+    function Isotope(z::Int)
+        isoRow = get_iso_row(z)
+        result = first(isoRow)
+        sym = result.Symbol
+        nme = result.Element
+        wkStr = result.Radioactive
+        if wkStr !== nothing 
+            active = parsebool(String(wkStr))
+        end
+        a = result.AtomicMass
+        new(z,a,sym,nme,active)
+    end
+end
+function decay!(iso::Isotope,radType::DecayType)
+    if radType == α
+        newIso = Isotope(iso.Z-2,iso.A-4)
+    elseif radType == β
+        newIso = Isotope(iso.Z+1,iso.A)
+    elseif radType == n
+        newIso = Isotope(iso.Z,iso.A-1)
+    else
+        newIso = Isotope(iso.Z,iso.A)
+    end
+    return newIso
+end
+
+
